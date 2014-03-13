91c87d1a57facd74c72bdd9a8a9eea9d26eab3df
terekacopeland <starsxmexico@live.com>
2014-03-13 14:14:47 -0400

    Addition

--- copeland_tereka_lab5.js
+++ copeland_tereka_lab5.js
@@ -1,1 +1,42 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Tereka Copeland
+//SDI 1403
+//Lab 5
+//Using Functions 
+
+//Variables
+var a;
+var b;
+var operation;
+var sum;
+var result
+var addNumber
+var subtractNumber
+var multiplyNumber
+var divideNumber
+parseInt(a);
+parseInt(b);
+
+a = prompt("Enter first number:");
+operation = prompt("ADD, SUBTRACT, MULTIPLY, DIVIDE? (+ = add, - = subtract, / = divide, * = multiply)");
+b = prompt("Enter second number:");
+ 
+ 
+ // Adding
+var addNumber = function(numOne, numTwo) {
+   
+   var result = numOne + numTwo;
+     return result;
+   
+ };
+
+ a = parseInt(a);
+ b = parseInt(b);
+ 
+ if(operation === "+") {
+   
+  sum = addNumber(a, b);
+   console.log(a + " + " + b + " = " + sum);
+   
+ };
